ea6c71e40de8b0fe5c1957669d90d9c1a7308d68
anddresMenchaca <141873677+anddresMenchaca@users.noreply.github.com>
2023-09-01 01:00:51 -0400

    Add Login_Interface PART_1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,21 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        [Route("/Session")]
+        public IActionResult Session()
+        {
+            return View();
+        }
+        [Route("/ProductCRUD")]
+        public IActionResult ProductCRUD()
+        {
+            return View();
+        }
+        [Route("/Users")]
+        public IActionResult Users()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Views/Home/ProductCRUD.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class ProductCRUDModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
