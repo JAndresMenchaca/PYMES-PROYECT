8705c24ad438e49be0708aa5a6e7174297aef7bf
anddresMenchaca <141873677+anddresMenchaca@users.noreply.github.com>
2023-09-14 12:39:14 -0400

    Add Home Widgets and CRUD Persons

---
+++ Models/Image.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models;
+
+public partial class Image
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public byte[] FilePath { get; set; } = null!;
+
+    public byte Status { get; set; }
+
+    public DateTime RegisterDate { get; set; }
+
+    public DateTime? LastUpdate { get; set; }
+
+    public int IdProduct { get; set; }
+
+    public int UserId { get; set; }
+
+    public virtual Product IdProductNavigation { get; set; } = null!;
+}
---
+++ Models/Specification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models;
+
+public partial class Specification
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string DataType { get; set; } = null!;
+
+    public string Value { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public byte Status { get; set; }
+
+    public DateTime RegisterDate { get; set; }
+
+    public DateTime? LastUpdate { get; set; }
+
+    public int IdProduct { get; set; }
+
+    public int UserId { get; set; }
+
+    public virtual Product IdProductNavigation { get; set; } = null!;
+}
