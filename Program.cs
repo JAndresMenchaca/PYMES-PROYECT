503b6daecf496f1286cf22c1ec638acd49d49bb3
anddresMenchaca <141873677+anddresMenchaca@users.noreply.github.com>
2023-10-02 09:32:05 -0400

    add_sprint2_part2

--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
+
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using WebApplication1.Models;
+using Proyecto_Pymes.Models.DB;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//importante
 builder.Services.AddDbContext<DbPymesContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+});
+
+
 
 var app = builder.Build();
 
@@ -27,9 +37,11 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseSession();
+
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
